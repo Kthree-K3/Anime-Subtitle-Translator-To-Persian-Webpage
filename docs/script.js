fc5757fd23beac479f0ea105106ec414d2e562bf
Kthree-K3 <khalilkhko@gmail.com>
2026-06-11 20:55:32 +0330

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1587,6 +1587,10 @@ async function getTranslationStream(fileUri, onChunk, onEnd, onError, abortSigna
             userFriendlyMessage = `<p>مدل انتخاب شده از حالت تفگر عمیق پشتیبانی نمی‌کند.</p>
             <pre>${errorMessageText}</pre>`;
             translationStatusMessage.innerHTML  = '❌ خطای تنظیمات';
+        } else if (errorMessageText.toLowerCase().includes('exceeded your current quota')) {
+            userFriendlyMessage = `<p>سهمیه روزانه کلید API شما به پایان رسیده است.</p>
+            <pre>${errorMessageText}</pre>`;
+            translationStatusMessage.innerHTML  = '❌ خطای سهمیه';
         } else if (errorMessageText.toLowerCase().includes('stream')) {
             userFriendlyMessage = `<p class="error-subtitle"><b>خطای دریافت ترجمه از پراکسی (Stream Error).</b></p><pre>${errorMessageText}</pre><p>این خطا معمولاً زمانی رخ می‌دهد که پراکسی فعال است و ارتباط شما با آن به دلیل ناپایداری اینترنت یا فیلترشکن دچار اختلال می‌شود.</p><p class="error-solution-title"><b>راه حل پیشنهادی:</b></p><ol><li>از اتصال پایدار اینترنت خود مطمئن شوید.</li><li>اگر از فیلترشکن همزمان با پراکسی استفاده می‌کنید، آن را موقتاً خاموش کرده و دوباره امتحان کنید (پراکسی خودش کار فیلترشکن را انجام می‌دهد).</li><li>اگر مشکل ادامه داشت، احتمالاً مشکل از سرورهای هوش مصنوعی است، چند دقیقه صبر کنید و بعد دوباره امتحان کنید.</li></ol>`;
             translationStatusMessage.innerHTML = '❌ خطای پراکسی/استریم.';
